b1afdc9f6d513d32d0dc9a847eec1b90d821fc7f
Angel Lopez Martin <anlo546@vidalibarraquer.net>
2025-10-25 17:08:09 +0200

    Actualizacion

---
+++ Bucle_Toxico/bucletoxico.py
@@ -0,0 +1,22 @@
+pareja = input("Me quieres? ").upper()
+amigos = 0
+while pareja not in ["SI","NO"]:
+    print("Por favor, indica si me quieres")
+    pareja = input("Me quieres? ").upper()
+
+if pareja == "SI":
+    amigos = input("Quedas con tus amigos? ").upper()
+    while amigos not in ["SI","NO"]:
+        print("Por favor, indica si quedas con tus amigos")
+        amigos = input("Quedas con tus amigos? ").upper()
+        if amigos == "SI":
+            print("Lo sabia, quedas sin decirme nada.")
+        else:
+            print("Ese es mi chico")
+else:
+    print("Lo sabia, es que eres un desgraciado")
+    tiempo = input("Hace cuanto tiempo que sientes que no me quieres? ")
+    print("Ojala te hagan lo mismo")
+
+
+
