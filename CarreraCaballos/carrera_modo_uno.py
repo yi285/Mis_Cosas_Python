50dba05e94b6e408959d410e6f747e579fe66d80
A.L.M <angellopez2006.24@gmail.com>
2025-11-08 22:37:26 +0100

    Add horse race simulation in carrera_modo_uno.py

---
+++ CarreraCaballos/carrera_modo_uno.py
@@ -0,0 +1,28 @@
+import random
+
+cero = random.randint(0,10)
+uno = random.randint(0,10)
+dos = random.randint(0,10)
+tres = random.randint(0,10)
+cuatro = random.randint(0,10)
+cinco = random.randint(0,10)
+seis = random.randint(0,10)
+siete = random.randint(0,10)
+ocho = random.randint(0,10)
+nueve = random.randint(0,10)
+diez = random.randint(0,10)
+
+
+
+print("---Resultado---")
+print("CABALLO0  :", "-" *cero ,cero )
+print("CABALLO1  :", "-" * uno, uno)
+print("CABALLO2  :", "-" * dos,dos)
+print("CABALLO3  :", "-" * tres,tres )
+print("CABALLO4  :", "-" * cuatro,cuatro)
+print("CABALLO5  :", "-" * cinco,cinco )
+print("CABALLO6  :", "-" * seis,seis )
+print("CABALLO7  :", "-" * siete,siete )
+print("CABALLO8  :", "-" * ocho,ocho )
+print("CABALLO9  :", "-" * nueve,nueve )    
+print("CABALLO10 :", "-" * diez,diez )
